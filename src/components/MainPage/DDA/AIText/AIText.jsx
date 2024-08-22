d49f2d9f684056847d855608f32d36024ca016c5
Etern1ty <Sandr1109@yandex.ru>
2024-08-22 23:15:03 +0400

    aitext

---
+++ src/components/MainPage/DDA/AIText/AIText.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './AIText.module.css';
+
+function AIText() {
+    return (
+        <div className={classes.aitext}>
+            AI-Driven Analytics Platform
+        </div>
+    )
+}
+
+export default AIText;
--- src/components/MainPage/DDA/DDA.jsx
+++ src/components/MainPage/DDA/DDA.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from './DDA.module.css';
 import DDDAsvg from "./DDDAsvg/DDDAsvg";
+import AIText from "./AIText/AIText";
 
 function DDA() {
     return (
         <div className={classes.dda}>
             <DDDAsvg />
+            <AIText />
         </div>
     )
 }
