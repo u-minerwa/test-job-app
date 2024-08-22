d298379bd76c63d58559d4a73598ab7d0554c185
Etern1ty <Sandr1109@yandex.ru>
2024-08-22 16:02:11 +0400

    reduxxx

---
+++ src/reduxxx/store.js
@@ -0,0 +1,19 @@
+let store = {
+    _state: {},
+
+    getState() { return this._state },
+
+    _callSubscriber() {
+        console.log("State changed");
+    },
+
+    observe(observer) {
+        this._callSubscriber = observer;
+    },
+
+    dispatchEvent(action) {
+        this._callSubscriber(this._state); 
+    }
+}
+
+export default store;
