b197b2c0b3b077124c782d8195339bcc9198f905
Etern1ty <Sandr1109@yandex.ru>
2024-08-23 00:32:36 +0400

    vf0vfvfv

--- src/components/MainPage/DDA/Rectangles/Rect1/Rect1.jsx
+++ src/components/MainPage/DDA/Rectangles/Rect1/Rect1.jsx
@@ -4,6 +4,7 @@ import classes from './Rect1.module.css';
 function Rect1() {
     return (
         <div className={classes.rect1}>
+            <img alt="rect1" src="/rect1data.jfif"></img>
         </div>
     )
 }
