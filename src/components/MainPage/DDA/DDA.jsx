287b2cdd65ea334db101c78c4f7b55df70ae8e66
Etern1ty <Sandr1109@yandex.ru>
2024-08-22 21:32:59 +0400

    fff4f4

--- src/components/MainPage/DDA/DDA.jsx
+++ src/components/MainPage/DDA/DDA.jsx
@@ -3,7 +3,9 @@ import classes from './DDA.module.css';
 
 function DDA() {
     return (
-        <div className={classes.dda}>DDA</div>
+        <div className={classes.dda}>
+            <img alt="bg" src='/ca18.png' ></img>
+        </div>
     )
 }
 
