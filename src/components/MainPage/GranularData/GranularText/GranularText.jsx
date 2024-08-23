0f91c670be74bf4956f5bcc1baee8aa70cbafd33
Etern1ty <Sandr1109@yandex.ru>
2024-08-23 19:03:13 +0400

    grtext

--- src/components/MainPage/GranularData/GranularText/GranularText.jsx
+++ src/components/MainPage/GranularData/GranularText/GranularText.jsx
@@ -3,7 +3,10 @@ import classes from './GranularText.module.css';
 
 function GranularText() {
     return (
-        <div className={classes.granularText}>Granular Txt</div>
+        <div className={classes.granularText}>
+            <div className={classes.redRect}></div>
+            <div className={classes.text}>Собираем и обрабатываем гранулярные данные на уровне сети</div>
+        </div>
     )
 }
 
