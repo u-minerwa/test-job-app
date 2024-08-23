8aee251c83feb5fbe500b005ec3f4455a4e7abbc
Etern1ty <Sandr1109@yandex.ru>
2024-08-23 13:51:07 +0400

    percents

--- src/components/MainPage/Pressure/FirstLayer/Percents/Percents.jsx
+++ src/components/MainPage/Pressure/FirstLayer/Percents/Percents.jsx
@@ -4,6 +4,8 @@ import classes from './Percents.module.css';
 function Percents() {
     return (
         <div className={classes.percents}>
+            <div className={classes.redRect}></div>
+            <div className={classes.text}>80% данных не используется производителями</div>
         </div>
     )
 }
