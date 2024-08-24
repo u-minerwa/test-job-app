c3520ac8679d19214577f62c63d65e27e34dd7cb
Etern1ty <Sandr1109@yandex.ru>
2024-08-24 18:30:46 +0400

    swipe dots

---
+++ src/components/MainPage/GranularData/GranularSwipe/DotsIndicator/DotsIndicator.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './DotsIndicator.module.css';
+
+const DotsIndicator = ({ activeIndex }) => {
+  return (
+    <div className={classes.dotsContainer}>
+      {[0, 1, 2].map(index => (
+        <span
+          key={index}
+          className={`${classes.dot} ${activeIndex === index ? classes.active : ''}`}
+        ></span>
+      ))}
+    </div>
+  );
+};
+
+export default DotsIndicator;
--- src/components/MainPage/GranularData/GranularSwipe/GranularSwipe.jsx
+++ src/components/MainPage/GranularData/GranularSwipe/GranularSwipe.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './GranularSwipe.module.css';
+import 'swiper/swiper-bundle.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import DotsIndicator from "./DotsIndicator/DotsIndicator";
 
 function GranularSwipe() {
+    const [activeIndex, setActiveIndex] = useState(0);
+
     return (
-        <div className={classes.granularSwipe}>Granular Swipe</div>
-    )
+        <div className={classes.swiper}>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                mousewheel={{ enabled: true }}
+                onSlideChange={(swiper) => {
+                    console.log('slide change', swiper.activeIndex);
+                    setActiveIndex(swiper.activeIndex); 
+                }}>
+                <SwiperSlide>Блок 1</SwiperSlide>
+                <SwiperSlide>Блок 2</SwiperSlide>
+                <SwiperSlide>Блок 3</SwiperSlide>
+            </Swiper>
+            <DotsIndicator activeIndex={activeIndex} />
+        </div>
+    );
 }
 
 export default GranularSwipe;
