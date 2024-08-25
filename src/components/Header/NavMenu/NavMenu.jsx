7387fe34f385e937171a2657be6a0b442bae24c6
Etern1ty <Sandr1109@yandex.ru>
2024-08-25 19:28:19 +0400

    fddff

--- src/components/Header/NavMenu/NavMenu.jsx
+++ src/components/Header/NavMenu/NavMenu.jsx
@@ -4,24 +4,16 @@ import { NavLink } from "react-router-dom";
 
 function NavMenu() {
     return (
-        <nav className={classes.navigationmenu}>
-            <div className={classes.item}>
-                <NavLink to='/mainpage'> Главная </NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to='/products'> Продукты </NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to='/cases'> Кейсы </NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to='/experts'> Экспертное мнение </NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to='/contacts'> Контакты </NavLink>
-            </div>
+        <nav>
+            <ul className={classes.navigationMenu}>
+                <li className={classes.item}><NavLink to="/mainpage"> Главная </NavLink></li>
+                <li className={classes.item}><NavLink to="/products"> Продукты </NavLink></li>
+                <li className={classes.item}><NavLink to="/cases"> Кейсы </NavLink></li>
+                <li className={classes.item}><NavLink to="/experts"> Экспертное мнение </NavLink></li>
+                <li className={classes.item}><NavLink to="/contacts"> Контакты </NavLink></li>
+            </ul>
         </nav>
-    )
+    );
 }
 
 export default NavMenu;
