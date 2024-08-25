dccb6d95fb45bd7f1b101a7a8df2cbf8bd5a9147
Etern1ty <Sandr1109@yandex.ru>
2024-08-25 18:27:29 +0400

    grfrr

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './Header.module.css';
 import NavMenu from "./NavMenu/NavMenu";
 import Prezentation from "./Prezentation/Prezentation";
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenuHandler = () => {
+        setIsMenuOpen((prevState) => !prevState);
+    };
+
     return (
         <header className={classes.header}>
-            <NavMenu />
+            <button className={classes.menuButton} onClick={toggleMenuHandler}> ☰ </button>
+            <div className={`${classes.navMenu} ${isMenuOpen ? classes.show : ''}`}>
+                <NavMenu />
+            </div>
             <Prezentation />
         </header>
-    )
+    );
 }
 
 export default Header;
